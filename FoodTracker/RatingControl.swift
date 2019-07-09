39d81d7c5a57ad9ca11aa38b410e0dbbfdede766
Jordan Dominguez <jordan.dominguez@treelineinteractive.com>
2019-07-09 11:05:47 -0700

    Adding a custon rating control class with a button (WIP)

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,44 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jordan Dominguez on 7/9/19.
+//  Copyright © 2019 Jordan Dominguez. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Set up the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+}
